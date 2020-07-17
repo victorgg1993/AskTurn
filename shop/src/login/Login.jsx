04a733c18b64198626d317676c0514b76867eb53
lockpicker <vigomga1993@gmail.com>
2020-07-18 00:06:50 +0200

    se ha implementado el login y la redirección al panel de gestión si ya estaba loggeado

--- shop/src/login/Login.jsx
+++ shop/src/login/Login.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
 import 'firebase/auth';
-import { useFirebaseApp } from 'reactfire';
-import { useHistory } from "react-router-dom";
+import { useFirebaseApp, useUser } from 'reactfire';
+import { useHistory, Redirect } from "react-router-dom";
 
 export default (props) => {
+
+    const usuario = useUser();
 
     const history = useHistory();
     const modulos_firebase = useFirebaseApp();
@@ -19,30 +21,42 @@ export default (props) => {
 
     const hacer_login = async () => {
 
-        console.log("falta implementar");
+        await modulos_firebase.auth().signInWithEmailAndPassword(email, password);
     }
 
     return (
         <div>
-            <p>login!</p>
-            
-            <p>
-                <label htmlFor="email">Correo electrónico:</label>
-                <input type="email" id="email" onChange={(evento) => setEmail(evento.target.value)} />
-            </p>
+            {
+                !usuario &&
+                <div>
+                    <p>login!</p>
 
-            <p>
-                <label htmlFor="password">Password:</label>
-                <input type="password" id="password" onChange={(evento) => setPassword(evento.target.value)} />
-            </p>
+                    <p>
+                        <label htmlFor="email">Correo electrónico:</label>
+                        <input type="email" id="email" onChange={(evento) => setEmail(evento.target.value)} />
+                    </p>
 
-            <p>
-                <button onClick={hacer_login} >Login</button>
-            </p>
+                    <p>
+                        <label htmlFor="password">Password:</label>
+                        <input type="password" id="password" onChange={(evento) => setPassword(evento.target.value)} />
+                    </p>
 
-            <p>
-                <button onClick={registro} >Registrarse</button>
-            </p>
+                    <p>
+                        <button onClick={hacer_login} >Login</button>
+                    </p>
+
+                    <p>
+                        <a onClick={registro} href="" >Registrarse</a>
+                    </p>
+                </div>
+            }
+
+            {
+                usuario &&
+                <div>
+                    <Redirect to={'/panel'} />
+                </div>
+            }
 
         </div>
     )

5f15083d49ca678347658374af6684ced4725efb
lockpicker <vigomga1993@gmail.com>
2020-07-17 16:47:53 +0200

    creado formulario de login

--- shop/src/login/Login.jsx
+++ shop/src/login/Login.jsx
@@ -1,29 +1,49 @@
 import React, { useState } from 'react';
 import 'firebase/auth';
 import { useFirebaseApp } from 'reactfire';
+import { useHistory } from "react-router-dom";
 
 export default (props) => {
 
+    const history = useHistory();
     const modulos_firebase = useFirebaseApp();
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
-    const submit = async () => {
-        //console.log(email, password);
-        await modulos_firebase.auth().createUserWithEmailAndPassword(email,password);
+    const registro = () => {
+
+        let path = `/register`;
+        history.push(path);
+    }
+
+    const hacer_login = async () => {
+
+        console.log("falta implementar");
     }
 
 
     return (
         <div>
-            <label htmlFor="email">Correo electrónico:</label>
-            <input type="email" id="email" onChange={(evento) => setEmail(evento.target.value)} />
+            <p>login!</p>
+            <p>
+                <label htmlFor="email">Correo electrónico:</label>
+                <input type="email" id="email" onChange={(evento) => setEmail(evento.target.value)} />
+            </p>
 
-            <label htmlFor="password">Password:</label>
-            <input type="password" id="password" onChange={(evento) => setPassword(evento.target.value)} />
+            <p>
+                <label htmlFor="password">Password:</label>
+                <input type="password" id="password" onChange={(evento) => setPassword(evento.target.value)} />
+            </p>
 
-            <button onClick={submit} >Login</button>
+            <p>
+                <button onClick={hacer_login} >Login</button>
+            </p>
+
+            <p>
+                <button onClick={registro} >Registrarse</button>
+            </p>
+
         </div>
     )
 }

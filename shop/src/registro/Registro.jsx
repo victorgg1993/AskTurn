d5a43bea56bea54c259195a69bca97659a976814
lockpicker <vigomga1993@gmail.com>
2020-07-17 20:28:25 +0200

    trabajando en la comprobación de la introducción correcta del email y password

--- shop/src/registro/Registro.jsx
+++ shop/src/registro/Registro.jsx
@@ -8,24 +8,43 @@ export default (props) => {
     const history = useHistory();
     const modulos_firebase = useFirebaseApp();
 
+    const [texto_mensaje, setMensaje] = useState('');
+
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [password_repetit, setPassword_repetit] = useState('');
+
 
 
     const registro = async () => {
-        // lo dejo apagado para no generar mil cuentas
-        //await modulos_firebase.auth().createUserWithEmailAndPassword(email, password);
-        console.log("apagado, ve a Registro.jsx, función registro y activa el await");
+
+        if ((password == null) || (password === "") || (password_repetit == null) || (password_repetit === "")) {
+            setMensaje("Error, casilla de password vacío");
+        }
+        else if (password.length < 6) {
+            setMensaje("Error, el password ha de contener 6 o más caracteres");
+        }
+        else if ((email == null) || (email === "")) {
+            setMensaje("Error, casilla de email vacío");
+        }
+        else {
+            // lo dejo apagado para no generar mil cuentas
+            //await modulos_firebase.auth().createUserWithEmailAndPassword(email, password);
+            console.log("Apagado, ve a Registro.jsx, función registro y activa el await.");
+            console.log("Correo: " + email + " password: " + password + "  password repetit: " + password_repetit);
+        }
     }
 
     const volver_login = () => {
-        let path = `/`; 
+        let path = `/`;
         history.push(path);
     }
 
     return (
         <div>
             <p>registro!</p>
+
+            <p id="id_mensajes_registro">{texto_mensaje}</p>
 
             <p>
                 <label htmlFor="email">Correo electrónico:</label>
@@ -38,11 +57,19 @@ export default (props) => {
             </p>
 
             <p>
+                <label htmlFor="password_repetit">Repite password:</label>
+                <input type="password" id="password_repetit" onChange={(evento) => setPassword_repetit(evento.target.value)} />
+            </p>
+
+
+
+
+            <p>
                 <button onClick={registro} >Registrarse</button>
             </p>
 
             <p>
-                <button onClick={volver_login} >volver al login</button>
+                <button onClick={volver_login} >Volver al login</button>
             </p>
         </div>
     )

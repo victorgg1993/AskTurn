aaac85291c990266752887467655616722b4affd
lockpicker <vigomga1993@gmail.com>
2020-07-17 18:19:46 +0200

    movido shop dentro de index.js porque era redundante

--- shop/src/index.js
+++ shop/src/index.js
@@ -1,15 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
-import Shop from './shop/Shop';
+
+import React, { Suspense } from 'react';
+import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import { FirebaseAppProvider } from 'reactfire';
 import firebaseConfig from './configuracion_firebase';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import Login from './login/Login'; // ventana de login del usuario
+import Registro from './registro/Registro'; // ventana de registro de usuario
+import Panel from './panel_gestion/Panel'; // panel que verá el usuario una vez loggeado
 
 ReactDOM.render(
   <React.StrictMode>
     <FirebaseAppProvider firebaseConfig={firebaseConfig}>
-      <Shop />
+
+      <Router>
+        <Switch>
+          
+          <Login path="/" exact component={Login} />
+
+
+          <Route path="/register">
+            <Suspense fallback={'Conectando...'}>
+              <div className="div_registro">
+                <Registro />
+              </div>
+            </Suspense>
+          </Route>
+
+
+          <Route path="/panel">
+            <Suspense fallback={'Conectando...'}>
+              <div className="div_panel_gestion">
+                <Panel />
+              </div>
+            </Suspense>
+          </Route>
+
+
+        </Switch>
+      </Router>
+
+
     </FirebaseAppProvider>
   </React.StrictMode>,
   document.getElementById('root')

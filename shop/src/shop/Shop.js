28e54a93f5fa2ca07262c7adc9a0eeb0e639abdc
lockpicker <vigomga1993@gmail.com>
2020-07-17 16:48:32 +0200

    añadido un route con su switch para poder gestionar mejor las páginas

--- shop/src/shop/Shop.js
+++ shop/src/shop/Shop.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import './Shop.css';
-import Login from '../login/Login'; // ventana de login
+import Login from '../login/Login'; // ventana de login del usuario
+import Registro from '../registro/Registro'; // ventana de registro de usuario
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function Shop() {
   return (
-    <div className="Shop">
-      <p>Usuario:</p>
-      <Login/>
-    </div>
+    <Router>
+      <Switch>
+        <Login path="/" exact component={Login} />
+
+        <Route path="/register">
+          <div className="div_registro">
+            <p>Usuario:</p>
+            <Registro />
+          </div>
+        </Route>
+
+      </Switch>
+    </Router>
   );
 }
 

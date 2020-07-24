00aa375282d6de164684aa6f3de4ac157c4e4e65
lockpicker <vigomga1993@gmail.com>
2020-07-24 14:05:44 +0200

    añadido redux

--- shop/src/index.js
+++ shop/src/index.js
@@ -2,51 +2,57 @@ import './index.css';
 
 import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
+
+import { Provider } from 'react-redux';
+import store from './redux';
+
 import { FirebaseAppProvider } from 'reactfire';
 import firebaseConfig from './configuracion_firebase';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Login from './login/Login'; // ventana de login del usuario
 import Registro from './registro/Registro'; // ventana de registro de usuario
 import Panel from './panel_gestion/Panel'; // panel que verá el usuario una vez loggeado
 
+
 ReactDOM.render(
   <React.StrictMode>
+    
     <FirebaseAppProvider firebaseConfig={firebaseConfig}>
+      <Provider store={store}>
+        <Router>
+          <Switch>
 
-      <Router>
-        <Switch>
+            <Route path="/" exact>
+              <Suspense fallback={'Conectando...'}>
+                <div className="div_login">
+                  <Login />
+                </div>
+              </Suspense>
+            </Route>
 
-        <Route path="/" exact>
-            <Suspense fallback={'Conectando...'}>
-              <div className="div_login">
-                <Login />
-              </div>
-            </Suspense>
-          </Route>
+            <Route path="/register">
+              <Suspense fallback={'Conectando...'}>
+                <div className="div_registro">
+                  <Registro />
+                </div>
+              </Suspense>
+            </Route>
 
-          <Route path="/register">
-            <Suspense fallback={'Conectando...'}>
-              <div className="div_registro">
-                <Registro />
-              </div>
-            </Suspense>
-          </Route>
+            <Route path="/panel">
+              <Suspense fallback={'Conectando...'}>
+                <div className="div_panel_gestion">
+                  <Panel />
+                </div>
+              </Suspense>
+            </Route>
 
-          <Route path="/panel">
-            <Suspense fallback={'Conectando...'}>
-              <div className="div_panel_gestion">
-                <Panel />
-              </div>
-            </Suspense>
-          </Route>
-
-        </Switch>
-      </Router>
-
-
+          </Switch>
+        </Router>
+      </Provider>
     </FirebaseAppProvider>
+    
   </React.StrictMode>,
   document.getElementById('root')
 );

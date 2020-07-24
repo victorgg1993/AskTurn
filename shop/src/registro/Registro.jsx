f769bacdbb33ea6052bf76f591a3fc7d602a72d0
lockpicker <vigomga1993@gmail.com>
2020-07-24 14:10:22 +0200

    se pasó a usar react-redux para controlar los estados de email,password, etc

--- shop/src/registro/Registro.jsx
+++ shop/src/registro/Registro.jsx
@@ -1,22 +1,28 @@
-import React, { useState } from 'react';
+//import React, { useState, useContext } from 'react';
+import React, { useState  } from 'react';
 import 'firebase/auth';
-import { useFirebaseApp, useUser } from 'reactfire';
+//import { useFirebaseApp, useUser } from 'reactfire';
+import { useFirebaseApp } from 'reactfire';
 import { useHistory } from "react-router-dom";
+import * as actions from '../redux/actions';
+import { useSelector, useDispatch } from 'react-redux';
 
 
 export default (props) => {
 
+    const dispatch = useDispatch();
+
     const history = useHistory();
     const modulos_firebase = useFirebaseApp();
-    const usuario = useUser();
+    //const usuario = useUser();
     const db = modulos_firebase.firestore();
 
-    const [texto_mensaje, setMensaje] = useState('');
+    const [texto_mensaje, setMensaje] = useState(''); // tira mensajes de error, aviso, etc
 
-    const [nom_usuari, setNombreUsuario] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [password_repetit, setPassword_repetit] = useState('');
+    const nom_usuari = useSelector(store => store.email);
+    const email = useSelector(store => store.email);
+    const password = useSelector(store => store.password);
+    const password_repetit = useSelector(store => store.password_repe);
 
     const esta_vacio = (dato) => {
         if (dato === "" || dato == null) {
@@ -25,10 +31,24 @@ export default (props) => {
         return false;
     }
 
+    const ir_panel = () => {
+        let path = `/panel`;
+        history.push(path);
+    }
+
+    const ir_login = (e) => {
+        e.preventDefault(); // para que no se vaya a "a_ningun_lado"
+        let path = `/`;
+        history.push(path);
+    }
+
     const registro = async () => {
 
         if (esta_vacio(password) || esta_vacio(password_repetit)) {
             setMensaje("Error, casilla de password vacío");
+        }
+        else if (password !== password_repetit) {
+            setMensaje("Error, los passwords no coinciden");
         }
         else if (password.length < 6) {
             setMensaje("Error, el password ha de contener 6 o más caracteres");
@@ -40,7 +60,7 @@ export default (props) => {
             // creamos usuario
             await modulos_firebase.auth().createUserWithEmailAndPassword(email, password).then(
                 () => {
-                    // creamos estructura de datos
+                    // creamos estructura de datos para ese usuario
                     const ref_tienda = db.collection('tienda');
                     ref_tienda.doc(email).set({
                         email: email,
@@ -48,7 +68,7 @@ export default (props) => {
                     });
                 }
             ).catch(
-                ()=>{
+                () => {
                     setMensaje("Error, el usuario ya existe!");
                 }
             );
@@ -56,39 +76,35 @@ export default (props) => {
             //console.log("Apagado, ve a Registro.jsx, función registro y activa el await.");
             // delay con algún mensaje de todo bien
             // después:
-            //volver_login();
+            ir_panel();
         }
     }
 
-    const volver_login = () => {
-        let path = `/`;
-        history.push(path);
-    }
 
     return (
         <div>
-            <p>registro!</p>
+            <p>Ventana de registro</p>
 
             <p id="id_mensajes_registro">{texto_mensaje}</p>
 
             <p>
                 <label htmlFor="usuario">Nombre de usuario:</label>
-                <input type="text" id="usuario" onChange={(evento) => setNombreUsuario(evento.target.value)} />
+                <input type="text" id="usuario" onChange={(evento) => dispatch(actions.darNombreUsuario(evento.target.value))} />
             </p>
 
             <p>
                 <label htmlFor="email">Correo electrónico:</label>
-                <input type="email" id="email" onChange={(evento) => setEmail(evento.target.value)} />
+                <input type="email" id="email" onChange={(evento) => dispatch(actions.darEmail(evento.target.value))} />
             </p>
 
             <p>
                 <label htmlFor="password">Password:</label>
-                <input type="password" id="password" onChange={(evento) => setPassword(evento.target.value)} />
+                <input type="password" id="password" onChange={(evento) => dispatch(actions.darPassword(evento.target.value))} />
             </p>
 
             <p>
                 <label htmlFor="password_repetit">Repite password:</label>
-                <input type="password" id="password_repetit" onChange={(evento) => setPassword_repetit(evento.target.value)} />
+                <input type="password" id="password_repetit" onChange={(evento) => dispatch(actions.darPasswordRepetido(evento.target.value))} />
             </p>
 
             <p>
@@ -96,7 +112,7 @@ export default (props) => {
             </p>
 
             <p>
-                <button onClick={volver_login} >Volver al login</button>
+                <a href="#a_ningun_lado" onClick={ir_login} >Volver al login</a>
             </p>
         </div>
     )

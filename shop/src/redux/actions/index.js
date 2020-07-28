017ffb9d90260ff3ca4527bbb890b2dca4eaff5f
lockpicker <vigomga1993@gmail.com>
2020-07-28 18:18:09 +0200

    se hizo limpieza, nada importante

--- shop/src/redux/actions/index.js
+++ shop/src/redux/actions/index.js
@@ -2,8 +2,26 @@ export const DAR_EMAIL = 'DAR_EMAIL';
 export const DAR_PASSW = 'DAR_PASSW';
 export const DAR_PASSW_REP = 'DAR_PASSW_REP';
 export const DAR_NOMBRE_USER = 'DAR_NOMBRE_USER';
+export const ADD_TICKETS = 'DAR_ARR_TICKETS';
 
-export const darEmail = (email) => ({ type: DAR_EMAIL, dada: email });
-export const darPassword = (password) => ({ type: DAR_PASSW, dada: password });
-export const darPasswordRepetido = (password_repe) => ({ type: DAR_PASSW_REP, dada: password_repe });
-export const darNombreUsuario = (nombre_usuario) => ({ type: DAR_NOMBRE_USER, dada: nombre_usuario });
+// Action creators
+export function darEmail(email) {
+    return { type: DAR_EMAIL, dada: email }
+}
+
+export function darPassword(password) {
+    return { type: DAR_PASSW, dada: password };
+}
+
+export function darPasswordRepetido(password_repe) {
+    return { type: DAR_PASSW_REP, dada: password_repe };
+}
+
+export function darNombreUsuario(nombre_usuario) {
+    return { type: DAR_NOMBRE_USER, dada: nombre_usuario };
+}
+
+
+export function addTicket(array_tickets) {
+    return { type: ADD_TICKETS, dada: array_tickets };
+}

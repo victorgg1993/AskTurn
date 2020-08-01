2220c9cc79d2170b5bb9dea53aec95a5f1bba55d
victorgg1993 <vigomga1993@gmail.com>
2020-08-01 23:29:21 +0200

    se añadió dar estado tanda, todavía no está

--- shop/src/redux/actions/index.js
+++ shop/src/redux/actions/index.js
@@ -2,8 +2,11 @@ export const DAR_EMAIL = 'DAR_EMAIL';
 export const DAR_PASSW = 'DAR_PASSW';
 export const DAR_PASSW_REP = 'DAR_PASSW_REP';
 export const DAR_NOMBRE_USER = 'DAR_NOMBRE_USER';
+export const DAR_ESTADO_TANDA = 'DAR_ESTADO_TANDA';
+
 export const ADD_TICKETS = 'DAR_ARR_TICKETS';
 export const FLUSH_TICKETS = 'FLUSH_TICKETS';
+
 
 // Action creators
 export function darEmail(email) {
@@ -21,6 +24,11 @@ export function darPasswordRepetido(password_repe) {
 export function darNombreUsuario(nombre_usuario) {
     return { type: DAR_NOMBRE_USER, dada: nombre_usuario };
 }
+
+export function darEstadoTanda(estado_tanda) {
+    return { type: DAR_ESTADO_TANDA, dada: estado_tanda };
+}
+
 
 
 export function addTicket(array_tickets) {

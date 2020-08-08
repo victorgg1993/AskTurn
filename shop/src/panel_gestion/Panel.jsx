a9100c44d68800212aac316c744020dbd4a7f44f
victorgg1993 <vigomga1993@gmail.com>
2020-08-08 16:57:41 +0200

    se implementaron las funciones crear tanda y eliminar tanda. Se tendrán que pulir algunos aspectos pero permite crear y borrar las tandas de firebase

--- shop/src/panel_gestion/Panel.jsx
+++ shop/src/panel_gestion/Panel.jsx
@@ -1,9 +1,11 @@
 //import React, { useState, useContext } from 'react';
-import React, { useEffect } from 'react';
+import './Panel.css';
+import React, { useState, useEffect } from 'react';
 import 'firebase/auth';
 import { useFirebaseApp, useUser } from 'reactfire';
 import { Redirect, useHistory } from "react-router-dom";
 import 'firebase/firestore';
+import * as firebase from 'firebase';
 import { useSelector, useDispatch } from 'react-redux';  // activar cuando no esté el debug activo
 //import { addTicket, flushTicket } from '../redux/actions';
 import { addTicket } from '../redux/actions';
@@ -12,6 +14,8 @@ const Panel = () => {
 
     //const _email = useSelector(store => store.email); // activar cuando no esté el debug activo
     const array_tickets = useSelector(store => store.funcionTickets);
+
+    const [n_tickets, setNumTickets] = useState(0);
 
     const history = useHistory();
     const dispatch = useDispatch();
@@ -26,21 +30,58 @@ const Panel = () => {
     }
 
     const crear_tanda = () => {
-    console.log("función crear tanda, falta implementar");
-    /*
-    // ------------------ temporal ( funciona )
-    const ref_tienda = db.collection(`tienda/` + usuario.email + `/ticket`);
-    ref_tienda.doc(`ticket_2`).set({
-    activo: false,
-    n_tanda_curso: 2,
-    });
-    // ------------------ temporal
+        // la magia ocurre en onClick_aceptar_tanda()
+        let modal = document.getElementById("id_modal_crear_tanda");
+        modal.style.display = "block";
+    }
 
-    */
+    const onClick_aceptar_tanda = () => {
+        
+        let nombre_tanda = document.getElementById("id_nombre_tanda").value;
+        console.log("nombre tanda", nombre_tanda);
+
+        const ref_tienda = db.collection(`tienda/` + usuario.email + `/ticket`);
+        ref_tienda.doc(`ticket_` + n_tickets).set({
+            activo: true,
+            date_final: firebase.firestore.Timestamp.fromDate(new Date()),
+            date_inicio: firebase.firestore.Timestamp.fromDate(new Date()),
+            n_tanda_curso: 1,
+            n_total_clientes: 2,
+            nombre: nombre_tanda,
+        }).then(
+            () => {
+                window.location.reload();
+            }
+        );
+    }
+
+    const onClick_cancelar_tanda = () => {
+
+        let modal = document.getElementById("id_modal_crear_tanda");
+
+        if (modal != undefined) {
+            modal.style.display = "none";
+        }
     }
 
     const borrar_tanda = () => {
-        console.log("función borrar tanda, falta implementar");
+
+        let puntero = document.getElementById(`id_lista_tickets`);
+
+        if (puntero.selectedIndex >= 0) {
+
+            let respuesta = window.confirm("Vas a eliminar la tanda. Aceptas?");
+
+            if (respuesta === true) {
+                db.collection(`tienda/` + usuario.email + `/ticket`).doc(`ticket_` + puntero.selectedIndex).delete()
+                    .then(function () {
+                        window.location.reload();
+                    })
+                    .catch(function (error) {
+                        console.error("Error removing document: ", error);
+                    });
+            }
+        }
     }
 
     const entrar_tanda = (evento, ticket, index) => {
@@ -48,7 +89,9 @@ const Panel = () => {
         evento.stopPropagation();
 
         let modal = document.getElementById('modal_tickets');
-        modal.show = 'display-block';
+        if (modal != null) {
+            modal.show = 'display-block';
+        }
 
         if (index < 0) {  // si no hay ninguna seleccionada, será la primera
             index = 0;
@@ -65,10 +108,13 @@ const Panel = () => {
 
             console.log("useEffect!");
 
-           //dispatch(flushTicket()); // vaciamos el array para tener los más nuevos de la DB
+            //dispatch(flushTicket()); // vaciamos el array para tener los más nuevos de la DB
 
             db.collection('tienda/' + _email + '/ticket').get().then(
                 (snapshot) => {
+
+                    setNumTickets(snapshot.size); // apuntamos el número de tickets que hay ( usado en crear_tanda() )
+
                     snapshot.forEach(
                         (doc) => {
                             dispatch(addTicket(doc.data()));
@@ -79,7 +125,7 @@ const Panel = () => {
                     console.log('Error recibiendo los tickets', err);
                 });
         }
-    }, [dispatch, db , usuario]) // el segundo parámetro está por esto: https://stackoverflow.com/questions/53070970/infinite-loop-in-useeffect#answer-53074436
+    }, [dispatch, db, usuario]) // el segundo parámetro está por esto: https://stackoverflow.com/questions/53070970/infinite-loop-in-useeffect#answer-53074436
 
     return (
         <div>
@@ -97,11 +143,20 @@ const Panel = () => {
                 <div>
                     <p>Bienvenido!</p>
 
-                    <div id="modal_tickets" className="modal display-block">
-                        <section className="modal-main">
-                            <button >close</button>
-                        </section>
+
+
+                    <div id="id_modal_crear_tanda" className="modal">
+                        <div className="modal-content">
+                            <form>
+                                <label>Nombre de la tanda:</label>
+                                <input type="text" id="id_nombre_tanda"></input><br />
+
+                                <button onClick={onClick_aceptar_tanda}>Aceptar</button>
+                                <button onClick={onClick_cancelar_tanda}>Cancelar</button>
+                            </form>
+                        </div>
                     </div>
+
 
                     <p>
                         <label>Elige un ticket:</label>
@@ -109,12 +164,11 @@ const Panel = () => {
 
                             array_tickets.map((ticket_actual, index) => (
                                 <option key={index} onDoubleClick={
-
                                     (evento) => {
-                                        entrar_tanda(evento, ticket_actual, index)
+                                        entrar_tanda(evento, ticket_actual, index);
                                     }}>
 
-                                    Nombre: {ticket_actual.nombre}   ID: {index}
+                                    Nombre: {ticket_actual.nombre}      ID: {index}
                                 </option>
                             ))
                         }

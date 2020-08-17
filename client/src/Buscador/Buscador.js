76d1b94958a45304f3d534ae6861c9723ce8ff9c
victorgg1993 <vigomga1993@gmail.com>
2020-08-17 17:38:22 +0200

    Añadida ventana de buscador de tickets, de momento vacía

---
+++ client/src/Buscador/Buscador.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Buscador.css';
+
+function Buscador() {
+  return (
+    <div className="Buscador">
+      hola mundo!
+    </div>
+  );
+}
+
+export default Buscador;

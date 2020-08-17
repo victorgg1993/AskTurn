edcd9b6d6c626005279dab5e4dcba6594a73ecdb
victorgg1993 <vigomga1993@gmail.com>
2020-08-17 17:37:45 +0200

    añadida estructura

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,61 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import React, { Suspense } from 'react';
+import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
+
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import { Provider } from 'react-redux';
+import store from './redux';
+
+import { FirebaseAppProvider } from 'reactfire';
+import firebaseConfig from './configuracion_firebase';
+
+
+import Buscador from './Buscador/Buscador';
+import VentanaTicket from './VentanaTicket/VentanaTicket';
+
+
+
+
+
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    
+    <FirebaseAppProvider firebaseConfig={firebaseConfig}>
+      <Provider store={store}>
+        <Router>
+          <Switch>
+
+
+            <Route path="/" exact>
+              <Suspense fallback={'Conectando...'}>
+                <div className="div_buscador">
+                  <Buscador />
+                </div>
+              </Suspense>
+            </Route>
+
+
+            <Route path="/ticket">
+              <Suspense fallback={'Conectando...'}>
+                <div className="div_VentanaTicket">
+                  <VentanaTicket />
+                </div>
+              </Suspense>
+            </Route>
+
+
+          </Switch>
+        </Router>
+      </Provider>
+    </FirebaseAppProvider>
+    
   </React.StrictMode>,
   document.getElementById('root')
 );
-
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
